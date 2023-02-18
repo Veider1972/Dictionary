7a7e2bc286a133fbffacb87928e8a1463cca20e3
Корховой Вадим <veider@inbox.ru>
2023-02-18 21:04:50 +0300

    Урок 07. Настройка Scope для DI. Repository по факту является делегатом DataSource...

--- app/src/main/java/ru/veider/dictionary/model/datasource/DataSourceImpl.kt
+++ app/src/main/java/ru/veider/dictionary/model/datasource/DataSourceImpl.kt
@@ -1,9 +1,14 @@
 package ru.veider.dictionary.model.datasource
 
+import org.koin.core.qualifier.named
+import org.koin.java.KoinJavaComponent.getKoin
+import ru.veider.dictionary.di.DATASOURCE_SCOPE
 import ru.veider.dictionary.model.data.Dictionary
 
-class DataSourceImpl(
-    private val provider: RemoteApi
-) : DataSource {
+class DataSourceImpl : DataSource {
+
+    private val scope = getKoin().createScope(this.toString(),named(DATASOURCE_SCOPE))
+    private val provider = scope.get<RemoteApi>()
+
     override suspend fun getDictionaryData(word: String): List<Dictionary> = provider.getDictionaryData(word)
 }
